c9ed8dc631eee91034f0f501e7e8607559f5402e
botbot <md.nowy@gmail.com>
2021-12-13 11:09:19 +0000

    init

---
+++ web.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Let the battle begin!');
+});
+
+app.post('/', function (req, res) {
+// console.error('Im here')
+//   console.log(req.body);
+//   const moves = ['F', 'T', 'L', 'R'];
+//   res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send('T');
+});
+
+app.listen(process.env.PORT || 8080);
